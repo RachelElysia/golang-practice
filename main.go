ccf9e7a2c76dac64f9ee6a1febe760508e9313f1
Rachel Perkins <rachelperkins@Rachels-MBP.lan>
2021-04-06 08:27:35 -0400

    Initialize comic book syntax

---
+++ main.go
@@ -0,0 +1,12 @@
+package main
+
+import "fmt"
+
+func main() {
+  var publisher, writer, artist, title string
+  var year, pageNumber uint
+  var grade float32
+
+  fmt.Println("Publisher:", publisher, "Writer:", writer, "Artist:", artist, "Title:",
+	title, "Year:", year, "Page Number:", pageNumber, "Grade:", grade)
+}
