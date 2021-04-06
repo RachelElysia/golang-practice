15e3f5d53ca8b46a40d7185a52174bb4c0622885
Rachel Perkins <rachelperkins@Rachels-MBP.lan>
2021-04-06 08:32:35 -0400

    Add and print variables

--- main.go
+++ main.go
@@ -7,6 +7,26 @@ func main() {
   var year, pageNumber uint
   var grade float32
 
+  title = "Mr. GoToSleep"
+  writer = "Tracey Hatchet"
+  artist = "Jewel Tampson"
+  publisher = "DizzyBooks Publishing Inc."
+  year = 1997
+  pageNumber = 14
+  grade = 6.5
+
   fmt.Println("Publisher:", publisher, "Writer:", writer, "Artist:", artist, "Title:",
-	title, "Year:", year, "Page Number:", pageNumber, "Grade:", grade)
+	title, "Year:", year, "Pages:", pageNumber, "Grade:", grade)
+
+  title = "Epic Vol. 1"
+  writer = "Ryan N. Shawn"
+  artist = "Pheobe Paperclips"
+  year = 2013
+  pageNumber = 160
+  grade = 9.0
+
+  fmt.Println("Publisher:", publisher, "Writer:", writer, "Artist:", artist, "Title:",
+	title, "Year:", year, "Pages:", pageNumber, "Grade:", grade)
+
+
 }
